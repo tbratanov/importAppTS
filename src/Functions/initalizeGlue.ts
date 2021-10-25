0e7566f9ceee676b5a9d764e96bb9230e3515724
Tsvetan Bratanov <tsvetan.bratanov@tick42.com>
2021-10-25 15:49:32 +0300

    Adding some changes to intializeGlue module

--- src/Functions/initalizeGlue.ts
+++ src/Functions/initalizeGlue.ts
@@ -1,9 +1,13 @@
 import Glue from "@glue42/desktop"
 
 export async function initializeGlue() {
-    try {
-        return Glue({layouts: "full"})
-    } catch(error) {
-        console.warn(error);
+    if((window as any).glue) {
+        return (window as any).glue
+    } else{
+        try {
+            return Glue({layouts: "full"})
+        } catch(error) {
+            console.warn(error)
+        }
     }
 }
