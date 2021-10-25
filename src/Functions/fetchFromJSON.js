4eca5c079e338f5e92cfb2d43466cda2cb9f551d
Tsvetan Bratanov <tsvetan.bratanov@tick42.com>
2021-10-25 14:15:32 +0300

    Cleaning up *.js files in src

--- src/Functions/fetchFromJSON.js
+++
@@ -1,9 +0,0 @@
-export async function fetchFromJSON(location) {
-    const response = await fetch(location);
-    if (!response.ok) {
-        throw new Error("error reading file" + response.status);
-    }
-    ;
-    return response.json();
-}
-;
